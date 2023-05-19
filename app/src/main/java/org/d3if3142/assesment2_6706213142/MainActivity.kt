0f39880c2150c82298358931b2650901dafbca81
rafivino <rafiesudrajat11@gmail.com>
2023-05-19 19:30:41 +0700

    initial commit

---
+++ app/src/main/java/org/d3if3142/assesment2_6706213142/MainActivity.kt
@@ -0,0 +1,18 @@
+package org.d3if3142.assesment2_6706213142
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
