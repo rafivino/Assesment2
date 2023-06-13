eca72f475bcb51ba50cf10d55906c77284f0fb9b
rafivino <91720183+rafivino@users.noreply.github.com>
2023-06-13 22:52:25 +0700

    Delete app/src/main/java/org/d3if3142/assesment2_6706213142/ui directory

--- app/src/main/java/org/d3if3142/assesment2_6706213142/ui/AboutFragment.kt
+++
@@ -1,6 +0,0 @@
-package org.d3if3142.assesment2_6706213142.ui
-
-import androidx.fragment.app.Fragment
-import org.d3if3142.assesment2_6706213142.R
-
-class AboutFragment : Fragment(R.layout.fragment_about)
