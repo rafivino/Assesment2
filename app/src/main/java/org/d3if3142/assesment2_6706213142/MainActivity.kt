85777e14c890dbf1f53c5c51a7804b8f4eef3ba1
rafivino <rafiesudrajat11@gmail.com>
2023-06-14 20:30:03 +0700

    initial commit

--- app/src/main/java/org/d3if3142/assesment2_6706213142/MainActivity.kt
+++ app/src/main/java/org/d3if3142/assesment2_6706213142/MainActivity.kt
@@ -2,6 +2,7 @@ package org.d3if3142.assesment2_6706213142
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -13,11 +14,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.i("MainActivity", "onCreate dijalankan")
+
         navController = findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart dijalankan")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainActivity", "onResume dijalankan")
+    }
+
+    override fun onPause() {
+        Log.i("MainActivity", "onPause dijalankan")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.i("MainActivity", "onStop dijalankan")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.i("MainActivity", "onDestroy dijalankan")
+        super.onDestroy()
     }
 }
